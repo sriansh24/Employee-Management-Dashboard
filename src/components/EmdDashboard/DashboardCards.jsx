8e4f6930db1716f4dbc8cf3fe6391b82a7152553
sriansh24 <pattanaiksrianshu@gmail.com>
2026-01-09 11:50:56 +0530

    Changing little bit UI for login page. Also fixed the UI bug in Dashboard page.

--- src/components/EmdDashboard/DashboardCards.jsx
+++ src/components/EmdDashboard/DashboardCards.jsx
@@ -3,9 +3,9 @@ import "../../assets/styles/Dashboard/dashboardcards.css"
 
 const DashboardCards = ({ total, active, inactive }) => {
     return (
-        <div className=" row row-cols-1 row-cols-md-3 g-3 mb-4 hand-pointer">
+        <div className=" row row-cols-1 row-cols-md-3 g-3 mb-4">
             <div className="col">
-                <div className="card shadow border-0 px-0">
+                <div className="card shadow border-0 px-0 dashboard-card">
                     <div className="card-body text-center">
                         <h6 className="text-muted">Total Employee</h6>
                         <h2 className="fw-bold">{total}</h2>
@@ -13,7 +13,7 @@ const DashboardCards = ({ total, active, inactive }) => {
                 </div>
             </div>
             <div className="col">
-                <div className="card shadow border-0 px-0">
+                <div className="card shadow border-0 px-0 dashboard-card">
                     <div className="card-body text-center">
                         <h6 className="text-muted">Active Employee</h6>
                         <h2 className="fw-bold text-success">{active}</h2>
@@ -21,7 +21,7 @@ const DashboardCards = ({ total, active, inactive }) => {
                 </div>
             </div>
             <div className="col">
-                <div className="card shadow border-0 px-0">
+                <div className="card shadow border-0 px-0 dashboard-card">
                     <div className="card-body text-center">
                         <h6 className="text-muted">Inactive Employee</h6>
                         <h2 className="fw-bold text-danger">{inactive}</h2>
--- src/pages/authpage/EmdLogin.jsx
+++ src/pages/authpage/EmdLogin.jsx
@@ -26,65 +26,63 @@ const EmdLogin = () => {
     };
 
     return (
-        <div className="emd-login-bg-color emd-login-wrapper">
-            <div className="container d-flex justify-content-center align-item-center">
-                <div className="card shadow p-5 emd-login-card">
-                    <h4 className="text-center mb-3">Employee Management Dashboard</h4>
+        <div className="container-fluid d-flex justify-content-center align-item-center">
+            <div className="shadow p-5 emd-login-card">
+                <h4 className="text-center mb-3 text-white">Employee Management Dashboard</h4>
 
-                    {error && <div className="alert alert-danger py-2">{error}</div>}
+                {error && <div className="alert alert-danger py-2">{error}</div>}
 
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-4 position-relative">
-                            <label htmlFor="email" className="form-label">Email address</label>
-                            <div className="input-group">
-                                <span className="input-group-text bg-white">
-                                    <Mail size={18} />
-                                </span>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    className="form-control"
-                                    placeholder='Email'
-                                    value={email}
-                                    onChange={(e) => {
-                                        setEmail(e.target.value)
-                                        setError('');
-                                    }}
-                                    required
-                                />
-                            </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4 position-relative">
+                        <label htmlFor="email" className="form-label">Email address</label>
+                        <div className="input-group input-box">
+                            <span className="input-group-text bg-white">
+                                <Mail size={18} />
+                            </span>
+                            <input
+                                type="email"
+                                id="email"
+                                className="form-control"
+                                placeholder='Email'
+                                value={email}
+                                onChange={(e) => {
+                                    setEmail(e.target.value)
+                                    setError('');
+                                }}
+                                required
+                            />
                         </div>
+                    </div>
 
-                        <div className="mb-4 position-relative">
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <div className="input-group">
-                                <span className="input-group-text bg-white">
-                                    <Lock size={18} />
-                                </span>
-                                <input
-                                    type={showPassword ? 'text' : 'password'}
-                                    id="password"
-                                    className="form-control"
-                                    placeholder='Password'
-                                    value={password}
-                                    onChange={(e) => {
-                                        setPassword(e.target.value)
-                                        setError('');
-                                    }}
-                                    required
-                                />
-                                <span
-                                    className="input-group-text bg-white hand-pointer"
-                                    onClick={() => setShowPassword(!showPassword)}
-                                >
-                                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
-                                </span>
+                    <div className="mb-4 position-relative">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <div className="input-group input-box">
+                            <span className="input-group-text bg-white">
+                                <Lock size={18} />
+                            </span>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                id="password"
+                                className="form-control"
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => {
+                                    setPassword(e.target.value)
+                                    setError('');
+                                }}
+                                required
+                            />
+                            <span
+                                className="input-group-text bg-white hand-pointer"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                            </span>
 
-                            </div>
                         </div>
-                        <button type="submit" className="btn btn-primary w-100">Login</button>
-                    </form>
-                </div>
+                    </div>
+                    <button type="submit" className="btn login-btn w-50">Login</button>
+                </form>
             </div>
         </div>
     );
